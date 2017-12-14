dc14911a752150d23f1879ef49458e9e34486ecd
Tagl <tagl@tagl.is>
2017-12-14 16:35:33 +0000

    Last commit was missing extern variables

--- PizzaButton.ino
+++ PizzaButton.ino
@@ -4,6 +4,11 @@
 const char* host = "api.dominos.is"; // Hostname for HTTP header
 const IPAddress ip(191, 235, 177, 30); // Using hostname for connection doesn't work, using ip does
 
+extern const char* ssid; // SSID for WiFi
+extern const char* password; // Password for WiFi
+extern const char* token; // API token
+extern const char* order_json; // JSON string for order
+
 void setup() {
   Serial.begin(115200);
   Serial.println("PizzaButton");
